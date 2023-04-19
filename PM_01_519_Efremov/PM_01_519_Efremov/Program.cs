cf1d3ec3a9f257887da7abd4f4058f9a8c1c23ae
pm02isip <130733873+pm02isip@users.noreply.github.com>
2023-04-19 17:34:48 +0300

    Add files via upload

---
+++ PM_01_519_Efremov/PM_01_519_Efremov/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM_01_519_Efremov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
